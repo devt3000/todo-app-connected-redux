a4546309bb050ac8f8e9a99616f534074aaa18b8
Stanley Makazhe <devt3000@gmail.com>
2018-09-29 10:40:55 +0200

    Added the mapStateToProps fn in VIsibilityFilters.js

--- src/components/VisibilityFilters.js
+++ src/components/VisibilityFilters.js
@@ -1,8 +1,8 @@
 import React from "react";
 import cx from "classnames";
 import { VISIBILITY_FILTERS } from "../constants";
-import {connect} from 'react-redux'
-import {setFilter} from '../redux/actions'
+import { connect } from "react-redux";
+import { setFilter } from "../redux/actions";
 
 const VisibilityFilters = ({ activeFilter }) => {
   return (
@@ -24,6 +24,10 @@ const VisibilityFilters = ({ activeFilter }) => {
       })}
     </div>
   );
+};
+
+const mapStateToProps = state => {
+  return { activeFilter: state.visibilityFilter };
 };
 
 export default VisibilityFilters;
